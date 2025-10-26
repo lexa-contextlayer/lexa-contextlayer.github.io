dd64d245eba40a0412b3ddb72558369362f4161f
Sai Raina <sai.raina@gmail.com>
2025-10-25 17:52:00 -0700

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: process.env.NODE_ENV === 'production' ? '/vite_react_shadcn_ts/' : '/',
   server: {
     host: "::",
     port: 8080,
