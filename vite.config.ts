8096789914485b6c87537058f45d60a80a0a2056
lexa-contextlayer <lexa.contextlayer@gmail.com>
2025-10-25 18:32:06 -0700

    Simplify deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,25 +3,12 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode, command }) => ({
+export default defineConfig({
   base: '/',
-  server: {
-    host: "::",
-    port: 8080,
-  },
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name]-[hash].js',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]'
-      }
-    }
-  }
-}));
+});
