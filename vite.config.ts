d47900b3225fb004456f8585ade362a7ae5dcc2d
lexa-contextlayer <lexa.contextlayer@gmail.com>
2025-10-25 18:06:12 -0700

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: process.env.NODE_ENV === 'production' ? '/vite_react_shadcn_ts/' : '/',
+export default defineConfig(({ mode, command }) => ({
+  base: '/',
   server: {
     host: "::",
     port: 8080,
